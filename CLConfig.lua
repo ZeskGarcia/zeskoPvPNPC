fa60ce024e59f24f0e17d4e25143b49bae85c61b
Zesk García Rodríguez Salaberría <116458300+ZeskGarcia@users.noreply.github.com>
2022-11-13 15:18:33 +0100

    Create CLConfig.lua

---
+++ CLConfig.lua
@@ -0,0 +1,22 @@
+Config = {} or Config
+
+Config.Locale = "es"
+
+Config.Locales = {
+  ['es'] = {
+    ['yes'] = "Sí",
+    ['no'] = "No",
+    ['bearings'] = "Rodamientos",
+    ['tochange'] = "Para cambiar",
+    ['movement'] = "Movimiento",
+    ['start'] = "Iniciado"
+  },
+  ['en'] = {
+    ['yes'] = "Yes",
+    ['no'] = "No",
+    ['bearings'] = "Bearings",
+    ['tochange'] = "To change",
+    ['movement'] = "Movement",
+    ['start'] = "Started"
+  }
+}
