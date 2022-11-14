ddd2055320af422fea2bff5b4e4fe637548bd570
Zesk García Rodríguez Salaberría <116458300+ZeskGarcia@users.noreply.github.com>
2022-11-14 08:55:09 +0100

    Update CLConfig.lua

--- CLConfig.lua
+++ CLConfig.lua
@@ -20,3 +20,18 @@ Config.Locales = {
     ['start'] = "Started"
   }
 }
+
+Config.Coords = vector3(33.2,-889.29,30.16)
+
+Config.Distance = 2
+
+Config.spawn = {
+  vector3(45.05, -869.3, 30.49),
+  vector3(41.22, -858.64, 30.63),
+  vector3(36.4, -866.06, 30.51),
+  vector3(39.49, -866.45, 30.51),
+  vector3(48.73, -864.81, 30.56),
+  vector3(48.05, -871.83, 30.46),
+  vector3(50.75, -858.9, 30.67),
+  vector3(41.98, -857.23, 30.66)
+}
